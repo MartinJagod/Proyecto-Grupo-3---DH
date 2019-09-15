bc517ad13ea7f6c5107072a75e43600bb5a37d3a
Martin Jagod <jagodnikmartin@gmail.com>
2019-09-15 18:45:53 -0300

    segundo commit

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+
+
   <a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo" width="100"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
