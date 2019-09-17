715195e3adca45077c49c50dfa1700e9f406f09b
Martin <martinfernandezercoli@gmail.com>
2019-09-17 13:15:29 -0300

    Nuevo home

--- home.php
+++ home.php
@@ -1,1 +1,282 @@
-PROBANDO HOME
+
+<html lang="en">
+  <head>
+    <!--    Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Underground</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700|Roboto&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/d74764b1ca.js"></script>
+</head>
+
+
+<body>
+
+<!-- ESTO ES  HOME -->
+
+
+
+<div class="container espaciado">
+	<div class="row">
+		<div class="col-md-12">
+
+
+<!-- ESTO ES CANVAS IZQUIERDO -->
+
+			<div class="row">
+				<div class="col-md-3">
+
+					  <img src="img\logo1.jpg" class="img-fluid" alt="Responsive image">
+
+					<ul class="list-unstyled">
+
+
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-home"></i>  Inicio</h5>
+              </a>
+						</li>
+
+            <li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-bell"></i>  Notificaciones</h5>
+              </a>
+						</li>
+
+
+            <li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-envelope"></i>  Mensajes</h5>
+              </a>
+						</li>
+
+            <li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-list-alt"></i> Listas</h5>
+              </a>
+            </li>
+
+            <li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-user-circle"></i> Perfil</h5>
+              </a>
+						</li>
+
+            <li class="list-item">
+              <a class="btn btn-link" href="#">
+              <h5><i class="fas fa-ellipsis-h"></i> Opciones</h5>
+              </a>
+            </li>
+          </ul>
+
+
+
+					<button type="button" class="btn btn-primary btn-sm">Chamullar</button>
+
+					</button>
+
+				</div>
+
+<!-- ESTO ES CANVAS CENTRAL -->
+
+
+				<div class="col-md-6 border">
+
+					<h2 class="border-bottom">
+						Inicio
+					</h2>
+
+
+
+          <!-- que estas haciendo -->
+
+                  <div class="container border-bottom fondoGris">
+
+          	<div class="row">
+          		<div class="col-md-12">
+          			<div class="row">
+          				<div class="col-md-3">
+          			 <i class="fas fa-user fa-6x"></i>
+          				</div>
+          				<div class="col-md-9">
+
+                    <form class="form-inline">
+                      <input class="form-control mr-sm-2" type="search" placeholder="Que estas haciendo?" aria-label="Search">
+                      <button class="btn btn-primary btn-lg my-2 my-sm-0" type="submit">Publicar</button>
+                    </form>
+
+
+
+          					<div class="row">
+          						<div class="col-md-6">
+          							<div class="row">
+          								<div class="col-md-4">
+
+          									<button type="button" class="btn btn-primary btn-sm">
+          										<i class="far fa-image fa-2x"></i>
+          									</button>
+          								</div>
+          								<div class="col-md-4 ">
+
+                            <button type="button" class="btn btn-primary btn-sm">
+          										<i class="far fa-smile fa-2x"></i>
+          									</button>
+          								</div>
+          								<div class="col-md-4">
+
+                            <button type="button" class="btn btn-primary btn-sm">
+          										<i class="fas fa-question fa-2x"></i>
+          									</button>
+          								</div>
+          							</div>
+          						</div>
+
+          					</div>
+          				</div>
+          			</div>
+          		</div>
+          	</div>
+          </div>
+
+
+
+
+
+
+<!-- se cierra canvas central -->
+				</div>
+
+
+
+<!-- ESTO ES CANVAS DERECHO -->
+<div class="col-md-3">
+
+<!-- buscador -->
+
+<nav class="navbar navbar-light bg-white">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+    <button class="btn btn-primary btn-sm my-2 my-sm-0" type="submit">Buscar</button>
+  </form>
+</nav>
+
+
+<div class="tendencias  fondoGris">
+            <h4>
+              Tendencias
+            </h4>
+					<ul class="list-unstyled">
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Fernet
+            </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Asado
+              </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Baile
+              </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Juntada
+              </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Europa
+              </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#EstadosUnidos
+              </a>
+						</li>
+						<li class="list-item border-bottom">
+              <a class="btn btn-link" href="#">
+							#Amigos
+              </a>
+						</li>
+
+					</ul>
+					<h3>
+						A quien seguir
+					</h3>
+					<ul class="list-unstyled">
+            <a class="btn btn-link" href="#">
+						<li class="list-item">
+							LuquitasJ
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							Leolondero
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							Elnegromaxi
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+              Elian
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							MartinFernan
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							MartinJagod
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							Charlychanta
+              </a>
+						</li>
+						<li class="list-item">
+              <a class="btn btn-link" href="#">
+							BarthSimp
+              </a>
+						</li>
+					</ul>
+				</div>
+
+
+
+
+			</div>
+		</div>
+	</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+<!--  FIN DE  HOME -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
