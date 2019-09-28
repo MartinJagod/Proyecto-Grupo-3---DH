9f2794cfef2c4c5df815e8280408881f995733b4
Martin Jagod <jagodnikmartin@gmail.com>
2019-09-28 15:41:46 -0300

    Ya controla el login, cargue en json ljagod@gmail.com clave lucasj njuarez@gmail.com nestorj y mfernandez@gmail.com clave martinf para probar

--- header.php
+++ header.php
@@ -1,25 +1,17 @@
 <header>
 
+  <head>
+    <!--    Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Underground</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:700|Roboto&display=swap" rel="stylesheet">
 
-  <a class="navbar-brand" href="#"><img src="img/logo.png" alt="Logo" width="100"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Registro</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
+    <script src="https://kit.fontawesome.com/d74764b1ca.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
 </header>
--- login.php
+++ login.php
@@ -1,30 +1,44 @@
 <html lang="en">
-  <head>
-    <!--    Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Underground</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-</head>
-
 
 <?php include ("./header.php"); ?>
 
+<?php include ("./validar.php"); ?>
 
 
-<div class="login container">
-<form>
-  <div class="container form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted"></small>
-  </div>
-  <div class="container form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Ingresar</button>
-</form>
+		<div class="container-fluid  col-md-12 banner principallogin ">
+			<div class="row col-md-12 banner ">
+
+				<div class=" container col-md-4 fondoGris">
+
+					<form role="form" action= "validar.php" method='post' >
+						<div class="form-group">
+							<label for="exampleInputEmail1">
+								Direccion de Email
+							</label>
+							<input type="email" class="form-control" name="email" id="email">
+						</div>
+
+						<div class="form-group">
+
+							<label for="exampleInputPassword1">
+								Password
+							</label>
+							<input type="password" class="form-control" name="password" id="password">
+						</div>
+
+						<button type="submit" class="btn btn-primary">
+							Submit
+						</button>
+					</form>
+				</div>
+
+				<div class="container shadow p-3 mb-5 bg-white rouded col-md-4 ">
+					<address>
+            <img class="col-12 banner " alt="Bootstrap Image" src="img/logo1.jpg" />
+            <br>
+						 <strong>Coordobeses, Inc.</strong><br /> Jose Baigorri 653.<br /> Cordoba, X5001AJM<br /> <abbr title="Phone">P:</abbr> (123) 456-7890
+					</address>
+				</div>
+			</div>
+
 </div>
